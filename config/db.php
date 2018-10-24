92372f78ae380a69f96482d5b3d8925ac23615bc
wangmin <503541843@qq.com>
2018-10-24 16:16:23 +0800

    修改配置文件

--- config/db.php
+++ config/db.php
@@ -2,11 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=sh_shop',
     'username' => 'root',
-    'password' => '',
+    'password' => 'wangmin',
     'charset' => 'utf8',
-
+    'tablePrefix' => 'sh_',
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
---
+++ views/layouts/mymain.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>模板</title>
+</head>
+<body>
+    <h2>my main  自己的模板</h2>
+    <?= $content ?>
+</body>
+</html>
